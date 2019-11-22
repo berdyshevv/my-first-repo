6b6aba37297ec78853eacef6b878a2b70ae23890
berdyshevv <viktorberdysh@gmail.com>
2019-11-22 14:36:42 +0200

    delete

--- hello_world.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h> // includes derectory
-
-int main() { // int means integer,and this function returns integer number,and this function is cald main and it has no arguments 
-	printf("Hello world"); // this line of code prints 'hello world'
-	return 0; // and this line of code returns number 0
-} // and this is the end of this function
